e3e5eb7e46c0e1fb521db3524a4169cf790198bc
loccaf <teejayx@gmail.com>
2016-08-25 19:03:59 +0100

    Part 7 completed

--- MusicVideo/Constants.swift
+++ MusicVideo/Constants.swift
@@ -11,3 +11,8 @@ import Foundation
 typealias JSONDictionary = [String: AnyObject]
 
 typealias JSONArray = Array<AnyObject>
+
+
+let WIFI = "WIFI Available"
+let NOACCESS = "NO Internet Access"
+let WWAN = "Cellular Access Available"
